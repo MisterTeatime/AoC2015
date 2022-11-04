333d417f14de2b2126dcea074f52080465070a3b
MisterTeatime <j.teatime@gmail.com>
2022-11-04 18:34:30 +0100

    Day01 Part 1

--- src/Template.kt
+++ src/Template.kt
@@ -1,7 +1,6 @@
-
 fun main() {
     fun part1(input: List<String>): Int {
-        return input[0].count{"(".contains(it) } - input[0].count { ")".contains(it) }
+        return input.size
     }
 
     fun part2(input: List<String>): Int {
@@ -10,10 +9,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 3)
+    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
-
